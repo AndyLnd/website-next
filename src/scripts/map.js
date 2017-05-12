2d1b4e91e78062603d434f0e30e88fd90c0cd2e4
moklick <info@moritzklack.com>
2017-05-12 17:27:53 +0200

    feat(map): add marker

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,20 +1,57 @@
-import mapboxgl from 'mapbox-gl'; 
+import mapboxgl from 'mapbox-gl';
+
+const webkidCoords = [13.422153, 52.500615];
 
 function init() {
   mapboxgl.accessToken = 'pk.eyJ1Ijoid2Via2lkIiwiYSI6ImNCUXNCYU0ifQ.JETFQZV_Fs0SPZqTgNtQ5A';
 
+
   const map = new mapboxgl.Map({
-    center: [13.422153, 52.500615],
+    center: webkidCoords,
     zoom: 13,
     minZoom: 10,
     maxZoom: 16,
+    pitch: 30,
     interactive: false,
     attributionControl: false,
     logoPosition: 'bottom-right',
     container: 'map',
     style: 'mapbox://styles/webkid/cj2lojjo7000v2smt2z4ne9w8'
   });
+
+  map.on('load', function () {
+
+    map.addLayer({
+      "id": "points",
+      "type": "symbol",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "FeatureCollection",
+          "features": [{
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": webkidCoords
+            }
+          }]
+        }
+      },
+      "layout": {
+        "icon-image": "circle-15",
+        "text-field": "webkid office",
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [.8, 0],
+        "text-anchor": "left",
+      },
+      "paint": {
+        "icon-color": '#ff0000',
+      }
+    });
+  });
+
 }
+
 
 export default {
   init
