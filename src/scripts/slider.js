1e09ea750bdd969c18dbbc967ce8bbff21b26ada
Christopher Möller <chrtze@gmail.com>
2017-05-16 12:25:32 +0200

    update slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -9,16 +9,31 @@ class Slider {
       return false;
     }
 
-    this.slider = new Swipe(this.container, {
-      startSlide: 0,
-      draggable: true,
-      transitionEnd: this.onSwipeChange
-    });
+    this.isMobile = window.innerWidth < 768;
 
+    this.init = this.init.bind(this);
     this.renderSlideControls = this.renderSlideControls.bind(this);
     this.changeSlide = this.changeSlide.bind(this);
+    this.onSlideChangeVideo = this.onSlideChangeVideo.bind(this);
+    this.onSlideChangeGif = this.onSlideChangeGif.bind(this);
+    this.getSlideByIndex = this.getSlideByIndex.bind(this);
 
+    this.onSlideChange = this.isMobile ? this.onSlideChangeGif : this.onSlideChangeVideo;
+
+    this.init();
     this.renderSlideControls();
+  }
+
+  init() {
+    this.slider = new Swipe(this.container, {
+      draggable: true,
+      callback: this.onSlideChange
+    });
+
+    const slideIndex = this.slider.getPos();
+    const currentSlide = this.getSlideByIndex(slideIndex);
+
+    this.onSlideChange(slideIndex, currentSlide);
   }
 
   renderSlideControls() {
@@ -37,9 +52,46 @@ class Slider {
     this.slider.slide(slideIndex, 600);
   }
 
-  onSwipeChange() {
+  onSlideChangeVideo(slideIndex, slide) {
+    // first stop all other videos
+    const playingSlides = this.container.querySelectorAll('.slide.playing');
+
+    [].forEach.call(playingSlides, slide => {
+      slide.classList.remove('playing');
+      const activeVideo = slide.querySelector('.bg-video');
+      activeVideo.pause();
+    });
+
     // load + play video in slide
+    const video = slide.querySelector('.bg-video');
+    const sources = video.querySelectorAll('source');
+
+    [].forEach.call(sources, source => {
+      source.setAttribute('src', source.getAttribute('data-src'));
+    });
+    
+    video.oncanplay = evt => {
+      video.play();
+      slide.classList.add('playing');
+    };
+
+    video.load();
   }
+
+  onSlideChangeGif(slideIndex, slide) {
+    const gif = slide.querySelector('.bg-gif');
+
+    gif.setAttribute('src', gif.getAttribute('data-src'));
+
+    gif.onload = () => {
+      slide.classList.add('playing');
+    }
+  }
+
+  getSlideByIndex(slideIndex) {
+    return this.container.querySelector(`.slide[data-index="${slideIndex}"]`);
+  }
+
 }
 
 export default Slider;
