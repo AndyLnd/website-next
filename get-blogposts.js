a0f4628ab472584c8e923b38c385eafc196b0113
Stefan Keil <an.stefan.keil@googlemail.com>
2017-05-16 13:02:33 +0200

    feat(blogposts) add integration for new blogpost feed

--- get-blogposts.js
+++ get-blogposts.js
@@ -1,4 +1,4 @@
-const RssParser = require('rss-parser');
+const axios = require('axios');
 const d3TimeFormat = require('d3-time-format');
 const fs = require('fs');
 const path = require('path');
@@ -9,22 +9,34 @@ const dateFormatter = d3TimeFormat.timeFormat('%d %B %Y');
 
 const result = [];
 
-RssParser.parseURL('https://blog.webkid.io/rss.xml', (err, data) => {
-  data.feed.entries.forEach(entry => {
-    result.push({
-      title: entry.title,
-      link: entry.link,
-      date: dateFormatter(new Date(entry.pubDate)),
-      text: entry.contentSnippet,
-      link: entry.link
+const getExcerpt = (text) => {
+  if (text !== '') {
+    return text.substr(0, 125) + '…';
+  }
+  return '…';
+}
+
+axios.get('https://blog.webkid.io/content.json')
+  .then(function (response) {
+    response.data.posts.forEach(entry => {
+      result.push({
+        title: entry.title,
+        link: entry.permalink,
+        date: dateFormatter(new Date(entry.date)),
+        text: getExcerpt(entry.text),
+        image: `${entry.permalink}${entry.image}`
+      })
     });
-  });
 
-  fs.writeFile(output, JSON.stringify(result), err => {
-    if (err) {
-      throw err;
-    }
+    fs.writeFile(output, JSON.stringify(result), err => {
+      if (err) {
+        throw err;
+      }
 
-    console.log('blogposts written to:', output);
+      console.log('blogposts written to:', output);
+    });
+  })
+
+  .catch(function (error) {
+    console.log(error);
   });
-});
