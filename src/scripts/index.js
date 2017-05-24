810fe380f674dd5721ad2c198a05e27b7a0b5f42
Stefan Keil <an.stefan.keil@googlemail.com>
2017-05-24 16:40:19 +0200

    feat(contact form) add async Submissions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,10 @@
 import Map from './map';
 import Slider from './slider';
 import Lazy from './lazy';
+import Contact from './contact';
 
 new Map('map');
 new Slider('slider');
 
 Lazy.init();
+Contact.init();
