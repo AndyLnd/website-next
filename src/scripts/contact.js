68b1f3c8bbdbe04a6a8438952de5e4c244b80614
Stefan Keil <an.stefan.keil@googlemail.com>
2017-05-24 18:12:50 +0200

    feat(contact form): add alert on submit

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -7,12 +7,23 @@ function init() {
 function addFormSubmitListener(el) {
   const contactForm = document.querySelector('.contact-form');
 
+
   if (contactForm) {
     contactForm.onsubmit = (e) => {
       e.preventDefault();
 
       ajax().post(contactForm.getAttribute('action'), { email: e.target.email.value, message: e.target.message.value, 'form-name': e.target['form-name'].value })
-        .then((response, error) => {console.log(response, error)})
+        .then((response, xhr) => {
+          contactForm.reset();
+
+          const alertEl = document.querySelector('.alert');
+          alertEl.className += " alert__success";
+
+          setTimeout(() => {alertEl.className = "alert";}, 3500);
+        })
+        .catch((responseError, xhr) => {
+          console.log(responseError);
+        })
     };
   }
 }
