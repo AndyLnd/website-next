40bbe51256822bd5307fa692044159f0fe766da8
Christopher Möller <chrtze@gmail.com>
2017-05-26 16:38:16 +0200

    fix form submit

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -14,7 +14,15 @@ function addFormSubmitListener(el) {
 
       const hiddenFieldValue = e.target['form-name'] ? e.target['form-name'].value : 'webkid-contact';
 
-      axios.post('/', { email: e.target.email.value, message: e.target.message.value, 'form-name': hiddenFieldValue })
+      axios.post('/', {
+        email: e.target.email.value,
+        message: e.target.message.value,
+        'form-name': hiddenFieldValue
+      }, {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
         .then(onSuccess)
         .catch(onError);
     };
