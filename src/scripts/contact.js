6b47c8bd5bda0644381f93a7734628f1f4e5e93a
errorroutine <tine@webkid.io>
2017-05-26 15:28:21 +0200

    fix(contact form): use axios for form submit

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,4 +1,4 @@
-import ajax from '@fdaciuk/ajax';
+import axios from 'axios';
 
 function init() {
   addFormSubmitListener();
@@ -13,8 +13,9 @@ function addFormSubmitListener(el) {
       e.preventDefault();
 
       const hiddenFieldValue = e.target['form-name'] ? e.target['form-name'].value : 'webkid-contact';
+      let test = contactForm.getAttribute('action');
 
-      ajax().post(contactForm.getAttribute('action'), { email: e.target.email.value, message: e.target.message.value, 'form-name': hiddenFieldValue })
+      axios.post(contactForm.getAttribute('action'), { email: e.target.email.value, message: e.target.message.value, 'form-name': hiddenFieldValue })
         .then(onSuccess)
         .catch(onError);
     };
