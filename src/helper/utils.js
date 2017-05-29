620cd104c37f9b132baeed05eea932612ecb3585
errorroutine <tine@webkid.io>
2017-05-29 16:38:25 +0200

    fix(portolio): sort projects correctly, sort highlighted projects by position attr

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,24 +1,39 @@
 const d3Collection = require('d3-collection');
 
 function sortProjects(file) {
-  const sortedByDate = file.sort(sortByDate);
+  //const sortedByDate = file.sort(sortByDate);
+  let highlightProjects = [];
+  let projects = [];
+  //separate highlight projects from regular ones
+  file.forEach((proj, i) => {
+    proj.isHighlight ? highlightProjects.push(proj) : projects.push(proj);
+  })
 
-  let highLightIndices = [];
-  sortedByDate.forEach((proj, i) => {
-    if (proj.isHighlight) {
-      highLightIndices.push(i)
+  projects = projects.sort(sortByDate);
+
+  //put projects in right order
+  let allProjects = [];
+  const gridSize = 8;
+  highlightProjects.sort(sortByPosition).forEach((proj, i) => {
+    allProjects.push(proj);
+    for(let j = 0; j < gridSize; j++){
+      allProjects.push(projects[(i * gridSize) + j]);
     }
-  });
-
-  highLightIndices.forEach((from, to) => {
-    // one highlighted project is followed by 8 none highlighted
-    sortedByDate.splice((to * 9), 0, sortedByDate.splice(from, 1)[0]);
-  });
-  return sortedByDate;
+  })
+  //add last projects to array
+  const projectsInArray = allProjects.length - highlightProjects.length;
+  if(projects.length > projectsInArray) {
+    allProjects = allProjects.concat(projects.splice(projectsInArray, projects.length - projectsInArray));
+  }
+  return allProjects;
 }
 
 function sortByDate(a, b) {
     return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+}
+
+function sortByPosition(a, b) {
+    return a.position - b.position;
 }
 
 function groupAwards(awards) {
